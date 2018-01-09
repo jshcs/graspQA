61cfc9d392ae5e4aa75cc3cae82ac76612584c8c
jshcs <jshcs@users.noreply.github.com>
2018-01-08 19:15:06 -0500

    merging with master

--- helpers/preprocessing.py
+++ helpers/preprocessing.py
@@ -7,12 +7,15 @@ def pre_process(file_name):
 	
     Pre processing the dataset. Load the dataset. 
     1. Reads the json file. 
-    2. Loads all the data into a list. 
+    2. Loads all the image_objects into a list.
     3. This method zips all the preprocessing material and return it. 
     """
 
-    data = json.load(open(file_name))
-    data = data['images']
+    # @TODO: Also store the Question type | Useful information to create Question-wise Stats
+
+    dataset = json.load(open(file_name))
+    print("Finished loading ", dataset["dataset"], " dataset")
+    image_objects = dataset['images']
 
     images = []
     questions = []
@@ -22,30 +25,26 @@ def pre_process(file_name):
     options3 = []
     options4 = []
 
-    for i in range(len(data)):
+    for image_object in image_objects:
 
-        qa_pairs, image_id, split, file_name = data[i].keys()
+        image_filename = image_object['filename']
+        qa_pairs = image_object['qa_pairs']
 
-        d = data[i]
-
-        dqa = d['qa_pairs']
-        image = d['filename']
-
-        for j in range(len(dqa)):
-            images.append(image)
-            questions.append(dqa[j]['question'])
-            answers.append(dqa[j]['answer'])
-            options1.append(dqa[j]['multiple_choices'][0])
-            options2.append(dqa[j]['multiple_choices'][1])
-            options3.append(dqa[j]['multiple_choices'][2])
-            options4.append(dqa[j]['answer'])
+        for qa_pair in qa_pairs:
+            images.append(image_filename)
+            questions.append(qa_pair['question'])
+            answers.append(qa_pair['answer'])
+            options1.append(qa_pair['multiple_choices'][0])
+            options2.append(qa_pair['multiple_choices'][1])
+            options3.append(qa_pair['multiple_choices'][2])
+            options4.append(qa_pair['answer'])
 
     return list(zip(images, questions, answers, options1, options2, options3, options4))
 
 
 def loadData(file_name):
     """
-    loadData loads the data, 
+    loadData loads the image_objects,
     1. Image
     2. Question
     3. Answer
